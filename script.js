a24ce9922d3488ca3407d79400061f28b7c10b72
Gurjit Singh <gurjitsinghisere@gmail.com>
2024-07-07 12:15:14 +0530

    improve ui , better note

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 let notes = document.querySelectorAll(".note");
 let sidebar = document.querySelector(".sidebar");
 
-function animation_bg(bgColor) {
-    const colorfulElement = document.querySelector(".colorful");
-    colorfulElement.style.backgroundColor = bgColor;
+async function animation_bg(bgColor) {
+  const colorfulElement = document.querySelector(".colorful");
+  colorfulElement.style.backgroundColor = bgColor;
+  const viewportHeight = window.innerHeight;
+  const randomTop = Math.random() * viewportHeight;
+  colorfulElement.style.top = `${randomTop}px`;
+  // Add the 'active' class after a short delay
+  setTimeout(() => {
+    colorfulElement.classList.add("active");
 
-    // Add the 'active' class after a short delay
+    // Remove the 'active' class after the animation duration (1s in this case)
     setTimeout(() => {
-      colorfulElement.classList.add("active");
-      
-      // Remove the 'active' class after the animation duration (1s in this case)
-      setTimeout(() => {
-        colorfulElement.classList.remove("active");
-      }, 900); // Match this duration to the animation duration in CSS
-    },0); // Adjust this delay as needed
-  }
-
-// change height of note
-notes.forEach((note) => {
-  let notewidth = note.getBoundingClientRect().width;
-  note.style.height = notewidth + "px";
-});
+      colorfulElement.classList.remove("active");
+    }, 900); // Match this duration to the animation duration in CSS
+  }, 0); // Adjust this delay as needed
+}
 
 let colorbtn_show = false;
 
 sidebar.addEventListener("mouseleave", (e) => {
   hidecolorbtns();
+});
+sidebar.addEventListener("mouseenter", (e) => {
+  showcolorbuttons();
 });
 
 let main_btn = document.querySelector(".add-note");
@@ -60,19 +59,28 @@ async function showcolorbuttons() {
 let colorbtns = document.querySelectorAll(".colors-btn");
 
 colorbtns.forEach((colorbtn) => {
-  colorbtn.addEventListener("click", (e) => {
-    let colorBtn = e.target;
-    let note = document.createElement("div");
-    note.classList.add("note");
-    note.style.backgroundColor = `#${colorbtn.getAttribute("id")}`;
-    let bgColor = getComputedStyle(colorBtn).backgroundColor;
-    animation_bg(bgColor);
-    document.querySelector(".notes-container").appendChild(note);
-    let notewidth = note.getBoundingClientRect().width;
-    note.style.height = notewidth + "px";
-    // Call the animation function with the background color
+    colorbtn.addEventListener("click", (e) => {
+      let colorBtn = e.target;
+      let note = document.createElement("div");
+      note.classList.add("note");
+      note.innerHTML = `
+              <div class="note-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. oiehfiuhefih oihiu iug</p>
+                <div class="note-bottom">
+                  <div class="date-div">Feb 23,2019</div>
+                  <button class="edit-note-btn"><img src="edit.svg" alt="edit-btn-svg"></button>
+                </div>
+              </div>`
+      note.style.backgroundColor = `#${colorbtn.getAttribute("id")}`;
+      let bgColor = getComputedStyle(colorBtn).backgroundColor;
+      animation_bg(bgColor);
+      let notesContainer = document.querySelector(".notes-container");
+      notesContainer.insertBefore(note, notesContainer.firstChild);
+      let notewidth = note.getBoundingClientRect().width;
+      note.style.height = notewidth + "px";
+    });
   });
-});
+  
 
 // hide color btns
 
